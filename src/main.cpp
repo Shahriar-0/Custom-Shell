d8236ad7a3f3ad0a7a717ce9ec3d4eedc26a5d18
Shahriar-0 <attarshahriar@gmail.com>
2024-12-10 02:03:27 +0330

    REPL

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,16 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  // so that the output is immediately visible
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    // so that the output is immediately visible
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+    while (true) {
+        std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": not found" << std::endl;
+        std::string input;
+        std::getline(std::cin, input);
+        std::cout << input << ": not found" << std::endl;
+    }
 }
